26b06db3daadffc4c1a485386b24c29cdceb6f28
Juliya Khanenko <jullietnm@gmail.com>
2022-09-17 22:48:41 +0300

    error 2 (1?)

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -10,14 +10,14 @@ export const fetchImages = async (inputValue, pageNr) => {
   
     `https://pixabay.com/api/?key=29908673-8e98a3b6b4897d6699e678186&q=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNr}`
   )
-    .then(async response => {
+    .then(response => {
       if (!response.ok) {
         if (response.status === 404) {
           return [];
         }
         throw new Error(response.status);
       }
-      return await response.json();
+      return  response.json();
     })
     .catch(error => {
       console.error(error);
--- src/js/index.js
+++ src/js/index.js
@@ -20,9 +20,13 @@ btnSearch.addEventListener('click', e => {
   e.preventDefault();
   cleanGallery();
   const trimmedValue = input.value.trim();
+
   if (trimmedValue !== '') {
-    fetchImages(trimmedValue, pageNumber).then(foundData => {
+
+    fetchImages(trimmedValue, pageNumber ).then(foundData => {
+  
       if (foundData.hits.length === 0) {
+        pageNumber >= 1;
         Notiflix.Notify.failure(
           'Sorry, there are no images matching your search query. Please try again.'
         );
@@ -33,6 +37,8 @@ btnSearch.addEventListener('click', e => {
         );
         btnLoadMore.style.display = 'block';
         gallerySimpleLightbox.refresh();
+   
+        
       }
     });
   }
@@ -42,16 +48,14 @@ btnLoadMore.addEventListener('click', () => {
   pageNumber++;
   const trimmedValue = input.value.trim();
   btnLoadMore.style.display = 'none';
-  fetchImages(trimmedValue, pageNumber).then(foundData => {
+  fetchImages(trimmedValue, pageNumber ).then(foundData => {
     if (foundData.hits.length === 0) {
       Notiflix.Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.'
       );
     } else {
       renderImageList(foundData.hits);
-      Notiflix.Notify.success(
-        `Hooray! We found ${foundData.totalHits} images.`
-      );
+     
       btnLoadMore.style.display = 'block';
     }
   });
